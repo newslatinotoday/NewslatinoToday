ec6939e053aedcf3754c0aa316cd42abd5288b24
marcosdevelo <marcosgomezda@gmail.com>
2019-12-03 02:08:49 +0000

    footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			Volver a la pagina principal © 2019 NewsLatinoToday - correo electronico: jkadsfljk@gmail.com - telefono:
+			(919)444-0000 - Locacion de oficina: Raleigh, NC
 		</p>
 	</footer>
 );
