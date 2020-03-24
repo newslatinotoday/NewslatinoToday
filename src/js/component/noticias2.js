4ed322dfa5ed05ed88027503189069c3a2ad1233
newslatinotoday <62478602+newslatinotoday@users.noreply.github.com>
2020-03-23 21:22:42 -0400

    Update noticias2.js

--- src/js/component/noticias2.js
+++ src/js/component/noticias2.js
@@ -42,18 +42,13 @@ export const NoticiasTwo = () => {
 						<br />
 						<div className="espacio3" />
 						<p className="parrafo">
-							El sueco, fiel a su estilo, le dijo adiós al conjunto de la MLS con un mensaje picante. A su
-							vez, agradeció el apoyo del club que lo hizo sentir vivo de nuevo.
+							
 							<br />
 							<br />
-							Zlatan Ibrahimovic anunció este jueves su despedida de Los Ángeles Galaxy, el equipo de la
-							Major League Soccer norteamericana en el que jugó dos temporadas, con un mensaje picante
-							para los hinchas, a los que les dijo que.
+							
 							<br />
 							<br />
-							Vine, vi y conquisté. Gracias por hacerme sentir vivo de nuevo. A los hinchas, quisieron
-							Zlatan y les di Zlatan. Son bienvenidos, la historia continúa. Ahora vuelvan a ver béisbol,
-							escribió el sueco, fiel a su estilo, en su cuenta de Twitter.
+							
 						</p>
 						<div className="espacio2" />
 						<Link to="/">
