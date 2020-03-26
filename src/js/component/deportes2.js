d6ecbb40d88993b324486c147abda71221634bad
newslatinotoday <62478602+newslatinotoday@users.noreply.github.com>
2020-03-26 18:42:20 -0400

    Update deportes2.js

--- src/js/component/deportes2.js
+++ src/js/component/deportes2.js
@@ -42,18 +42,15 @@ export const DeportesTwo = () => {
 						<br />
 						<div className="espacio3" />
 						<p className="parrafo">
-							{/* El sueco, fiel a su estilo, le dijo adiós al conjunto de la MLS con un mensaje picante. A su
-							vez, agradeció el apoyo del club que lo hizo sentir vivo de nuevo. */}
+		Un cordial saludopara los Señores DT de equipos , Jugadores y Familias ; se les informa que este fin de semana quedan suspendidas las jornadas de sábado ( Veteranos ) y domingo ( libre ) , la razón pues todos ya lo sabemos y por orden de las autoridades locales, el fútbol se reanudará, hasta nuevo aviso, lo siento por el inconveniente .
+				
+							<br />
+		Tranquilos que "la pelota no se mancha".
+							<br />
+							{/* */}
 							<br />
 							<br />
-							{/* Zlatan Ibrahimovic anunció este jueves su despedida de Los Ángeles Galaxy, el equipo de la
-							Major League Soccer norteamericana en el que jugó dos temporadas, con un mensaje picante
-							para los hinchas, a los que les dijo que. */}
-							<br />
-							<br />
-							{/* Vine, vi y conquisté. Gracias por hacerme sentir vivo de nuevo. A los hinchas, quisieron
-							Zlatan y les di Zlatan. Son bienvenidos, la historia continúa. Ahora vuelvan a ver béisbol,
-							escribió el sueco, fiel a su estilo, en su cuenta de Twitter. */}
+							{/*  */}
 						</p>
 						<div className="row row1 spacewithmain">
 							<div className="col-sm-4">
