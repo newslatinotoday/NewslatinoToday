b99a893625dab557266f91c71ef0d022a70236a1
Marcos Gomez <mdgdevelop@gmail.com>
2020-04-13 03:22:21 +0000

    logos fixed

--- src/js/component/sponsors.js
+++ src/js/component/sponsors.js
@@ -46,7 +46,7 @@ export const Sponsors = () => {
 						<div className="col-md-4">
 							<a href="https://m.facebook.com/LigaHispanaDeBunn/">
 								<img
-									src="https://scontent.fmia1-1.fna.fbcdn.net/v/t31.0-8/22181450_1354862417945008_6077301093248005771_o.jpg?_nc_cat=108&_nc_ohc=2h61ukek2-0AQktih3uNzaWdG8a9r9iJ8SWdSLzrZ-Smr0iXltIdRfqBw&_nc_ht=scontent.fmia1-1.fna&oh=6ac46f94afddb4946fd4e6dfb364ac89&oe=5E7C4886"
+									src="https://scontent.fmia1-1.fna.fbcdn.net/v/t31.0-8/22181450_1354862417945008_6077301093248005771_o.jpg?_nc_cat=108&_nc_sid=85a577&_nc_oc=AQnRcDkHiuEEF-BZ0L33C9zCWLyIf-qxUhXSBqCapU8wRJigV4XxxG6oTPb2v1noy-v7SpQakF8ol6NH80VyIcDp&_nc_ht=scontent.fmia1-1.fna&oh=5517f994295305f264295c98566bb96f&oe=5EB98EFA"
 									className="card-img"
 									alt="..."
 								/>
@@ -81,12 +81,12 @@ export const Sponsors = () => {
 				</div>
 
 				<div className="espacio" />
-				<div className="card mb-3 cardstrap">
+				{/* <div className="card mb-3 cardstrap">
 					<div className="row no-gutters">
 						<div className="col-md-4">
 							<a href="https://m.facebook.com/ElMolcajeteMexicanRestaurant/">
 								<img
-									src="https://scontent.fmia1-1.fna.fbcdn.net/v/t1.0-9/fr/cp0/e15/q65/51604189_2843241429023173_3996615399918534656_n.jpg?_nc_cat=109&_nc_ohc=gYV4yNz0m50AQlQF5z9qqPnHiPTl5Dr_rC-Pw5uiWKuB656hoEuxQx4rA&_nc_ht=scontent.fmia1-1.fna&oh=1e43f37a6a75b45656802ef438f81b43&oe=5E81F6A0"
+									src=""
 									className="card-img"
 									alt="..."
 								/>
@@ -98,14 +98,14 @@ export const Sponsors = () => {
 							</div>
 						</div>
 					</div>
-				</div>
+				</div> */}
 				<div className="espacio" />
 				<div className="card mb-3 cardstrap">
 					<div className="row no-gutters">
 						<div className="col-md-4">
 							<a href="https://hopecafe.church/">
 								<img
-									src="https://scontent.fmia1-2.fna.fbcdn.net/v/t1.15752-9/s2048x2048/82597766_656709181737457_2370873246481383424_n.png?_nc_cat=111&_nc_oc=AQlXEYOJa6SZGdI6mA6vHsTJ_CfY25whAILLWReJlvlrPi1_PBlJXX6Cs9sel2YFOm9WnV8nnc8muhxWuYAvCIjI&_nc_ht=scontent.fmia1-2.fna&oh=76e168ea0815ff44c0597c2fda47d749&oe=5E8DE384"
+									src="https://scontent.fmia1-2.fna.fbcdn.net/v/t1.15752-9/s2048x2048/82597766_656709181737457_2370873246481383424_n.png?_nc_cat=111&_nc_sid=b96e70&_nc_oc=AQkkKSKCrtSpDJO9tmPNCtUvz1CXl4r-qOqkdD-9nyOyIIJ7_QEL50sdoYIJKZYpAWQIg8oluaS0HgsSk66UgEni&_nc_ht=scontent.fmia1-2.fna&oh=182c3b6d2b054744b4eab7e11d949d0e&oe=5EB7C5FC"
 									heigjjght="200"
 									height="170"
 									className="card-img"
@@ -130,7 +130,7 @@ export const Sponsors = () => {
 						<div className="col-md-4">
 							<a href="https://www.aarp.org/espanol/">
 								<img
-									src="https://www.2ndvote.com/wp-content/uploads/2016/03/4973142632038400.jpg"
+									src="https://static.wixstatic.com/media/14b520_decc8a90626c45689c0a5aea1f5113f2~mv2.png/v1/fill/w_800,h_439,al_c,q_90/file.jpg"
 									className="card-img"
 									alt="..."
 								/>
